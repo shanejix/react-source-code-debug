1e7c8380723fc2f760cb473ce1ef7e68678affaf
shanejix <shanejix@hotmail.com>
2022-04-11 16:40:01 +0800

    feat: ignore js ts lint

--- src/react/.eslintrc.js
+++ src/react/.eslintrc.js
@@ -11,7 +11,9 @@ const OFF = 0;
 const ERROR = 2;
 
 module.exports = {
-  extends: ['fbjs', 'prettier'],
+  // FIXME: REACT SOURCE CODE DEBUG
+
+  // extends: ['fbjs', 'prettier'],
 
   // Stop ESLint from looking for a configuration file in parent folders
   root: true,
